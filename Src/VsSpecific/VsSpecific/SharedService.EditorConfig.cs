9fce3906819c666f032e565c2788ad3182cd66ea
Rick Sladkey <rick.sladkey@gmail.com>
2018-04-30 15:13:17 -0400

    Support editor config on VS2017 or later

--- Src/VsSpecific/VsSpecific/SharedService.EditorConfig.cs
+++ Src/VsSpecific/VsSpecific/SharedService.EditorConfig.cs
@@ -3,22 +3,25 @@ using Microsoft.VisualStudio.Text.Editor.OptionsExtensionMethods;
 
 namespace Vim.VisualStudio.Specific
 {
-#if VS_SPECIFIC_2017
-
-    internal partial class SharedService 
-    {
-        private bool GetInsertFinalNewLine(IEditorOptions options)
-        {
-            return DefaultOptionExtensions.GetInsertFinalNewLine(options);
-        }
-    }
-#else
+#if VS_SPECIFIC_2012 || VS_SPECIFIC_2013 || VS_SPECIFIC_2015
 
     internal partial class SharedService 
     {
         private bool GetInsertFinalNewLine(IEditorOptions options)
         {
             return false;
+        }
+    }
+
+#else
+
+    // Implement EditorConfig related methods only supported on
+    // VS2017 or later.
+    internal partial class SharedService 
+    {
+        private bool GetInsertFinalNewLine(IEditorOptions options)
+        {
+            return DefaultOptionExtensions.GetInsertFinalNewLine(options);
         }
     }
 
